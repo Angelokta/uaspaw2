a1e9af385d02ec8d2bcb22491d5eb59bc5838710
Angelia2010 <angeliaoktarini20@gmail.com>
2026-01-10 23:19:05 +0700

    welcome

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
+// Import library Express
 var express = require('express');
+
+// Membuat instance router dari Express
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/*
+  GET Home Page
+  Route utama saat user mengakses '/'
+*/
+router.get('/', function (req, res, next) {
+
+  // Render file view 'index'
+  // Mengirim data ke view agar bisa ditampilkan dinamis
+  res.render('index', {
+    title: 'Welcome to Novel App',
+  });
+
 });
 
+// Export router agar bisa digunakan di app.js
 module.exports = router;
